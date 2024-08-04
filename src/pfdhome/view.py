f0c2baba531cd387928f468f5c203652e2dd2327
phuchh <phuchh27@gmail.com>
2024-08-04 15:51:23 +0700

    init project

---
+++ src/pfdhome/view.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+import pathlib
+from django.shortcuts import render
+
+from visits.models import PageVisit
+this_dir = pathlib.Path(__file__).resolve().parent
+
+
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_ps = PageVisit.objects.filter(path=request.path)
+    my_title = "Home"
+    my_context = {
+        "page_title": my_title,
+        "queryset_count": page_ps.count(),
+        'total_visit_count': qs.count()
+    }
+    html_tamplate = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_tamplate, my_context)
