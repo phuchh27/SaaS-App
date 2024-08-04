2e1da63f8d1e8d3829c2e4f1a6a577f2eb1fab60
phuchh <phuchh27@gmail.com>
2024-08-04 17:16:38 +0700

    add Dijango debug environ var

--- src/pfdhome/settings.py
+++ src/pfdhome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,9 @@ print("BASE_DIR", BASE_DIR)
 SECRET_KEY = 'django-insecure-xwt(fiptxgmsvyq5+29-a8ao^-rtrpw-vf)pcl(@-mx@dgblo)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == 'true' 
+
+print("DEBUG: ", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app"  # https://saas.prod.railway.app
